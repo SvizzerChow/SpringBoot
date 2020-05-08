a785c847217210f39bc757ccfd405d443e3a0bee
SvizzerChow <mysvizzera@outlook.com>
2020-05-08 19:07:45 +0800

    init

---
+++ SpringBoot/normal/src/main/java/ink/chow/boot/normal/NormalApplication.java
@@ -0,0 +1,13 @@
+package ink.chow.boot.normal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalApplication.class, args);
+    }
+
+}
